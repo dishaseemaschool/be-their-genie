39b4ed057f956a91ff641e0804e66ab0c281dcca
Vivek Aithal K R <nuwanda@Viveks-MacBook-Air.local>
2016-09-24 01:42:33 +0530

    update images

--- jsmain.js
+++ jsmain.js
@@ -1,4 +1,22 @@
 var wishlist = [{
+    'title': 'Eggs',
+    'descrip': 'Eggs, twice a week, is necessary for the growth of kids. Even if given eggs half a year, twice a week, the cost runs to around 50k',
+    'units_needed': '',
+    'link':'',
+    'units_fulfilled': '',
+    'cost': '',
+    'status': 0,
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Food-eggs-box_%2824326315095%29.jpg'
+}, {
+    'title': 'Mosquito Net',
+    'descrip': '6.5*6.5 sq. ft. mosquito nets for the kids to sleep in. This is an important necessity, especially during the monsoon season.',
+    'link': 'http://www.amazon.in/Blue-Classic-Heavy-Delux-Mosquito/dp/B01EJU6DQG/ref=sr_1_2?ie=UTF8&qid=1462739531&sr=8-2-spons&keywords=mosquito+net&psc=1',
+    'units_needed': 4,
+    'units_fulfilled': 0,
+    'cost': 489.00,
+    'status': 0,
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/2/23/Malaria_prevention-Insecticide_treated_bed_net-PMI.jpg'
+},{
     'title': 'Sound Box',
     'descrip': 'Two soundboxes for annoucements in dormitories, announcements during morning assembly, sports and other engagements',
     'link': 'http://www.amazon.in/Philips-Multimedia-Speakers-SPA1260-12/dp/B007VZFZO8?ie=UTF8&keywords=stereo%20speakers&qid=1462739125&ref_=sr_1_5&sr=8-5',
@@ -6,149 +24,131 @@ var wishlist = [{
     'units_fulfilled': 0,
     'cost': 1168.00,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/4/4b/G%C5%82o%C5%9Bniki.JPG'
     }, {
-    'title': 'Mosquito Net',
-    'descrip': '6.5*6.5 sq. ft.',
-    'link': 'http://www.amazon.in/Blue-Classic-Heavy-Delux-Mosquito/dp/B01EJU6DQG/ref=sr_1_2?ie=UTF8&qid=1462739531&sr=8-2-spons&keywords=mosquito+net&psc=1',
-    'units_needed': 4,
-    'units_fulfilled': 0,
-    'cost': 489.00,
-    'status': 0,
-    'imageURL': 'imgs/5.jpg'
-}, {
     'title': 'Steel Almirah',
-    'descrip': '',
+    'descrip': 'A general purpose cupboard is needed, to store things of value',
     'link': 'http://www.flipkart.com/durian-alden-b-stainless-steel-collapsible-wardrobe/p/itmeg4ug49zrufqr?pid=CWDEG4UGAVQHVVFV&al=SjMsdDNFPsWUdjJlkNmJi8ldugMWZuE75aUsiwTbcEM6Re4IzNLHsYiZKpH4wXUoFooWMqSTJgI%3D&ref=L%3A7915270700945263890&srno=p_18&findingMethod=Search&otracker=start',
     'units_needed': 2,
     'units_fulfilled': 0,
     'cost': 16575,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/4/46/Alamari.jpg'
 }, {
     'title': 'CFL Lights',
-    'descrip': '',
+    'descrip': 'Energy efficient CFL lights are needed to, as there are no streetlights nearby. Lights are needed in the dormitories and corridors.',
     'link': 'http://www.flipkart.com/aae-20-w-cfl-eurolex-bulb/p/itmegggefyd8bmfr?pid=BLBEGGGEG9Z2N5AE&al=SjMsdDNFPsV%2FPzivkln5t8ldugMWZuE7mxWx381qOwQa%2FjQIklnJgm7vnBP%2FpPGoBNq388G1Nq4%3D&ref=L%3A-7182202938594606719&srno=p_1&otracker=from-search',
-    'units_needed': 2,
+    'units_needed': 5,
     'units_fulfilled': 0,
     'cost': 200,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/b/bb/CFL-grow-light-dual-spectrum.jpg'
 }, {
     'title': 'Large Kadai',
-    'descrip': 'Cooking Utensil 15+ L',
+    'descrip': 'Cooking Utensil 15+ L, for hassle-free cooking',
     'units_needed': 1,
     'units_fulfilled': 0,
     'cost': 1300,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Kadai.jpg'
 }, {
 
     'title': 'Large Dekchi',
-    'descrip': 'Cooking Utensil 15+ L',
+    'descrip': 'Cooking Utensil 15+ L, much needed in the kitchen',
     'units_needed': 1,
-    'units_fulfilled': 3,
+    'units_fulfilled': 0,
     'cost': 500,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Kadai_and_a_dekchi.JPG'
 }, {
     'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'descrip': 'The kids play cricket, and new bats would be lovely',
+    'units_needed': 4,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/c/c6/A_modern_Cricket_bat_%28back_view%29.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Bharatanatyam Costumes',
+    'descrip': 'The kids are learning Bharatanatyam and intend to have shows either in the premises or during IITKGP events. Costumes would encourage and motivate them to embrace the art',
+    'units_needed': 4,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Bharata_Natyam_Performance_DS.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Make-up kit',
+    'descrip': 'Needed for special events when the kids put up shows',
+    'units_needed': 1,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/1/16/Makeup_Kit_5-5-09_IMG_3543.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'DVD Player',
+    'descrip': 'A DVD player is necessary for general entertainment purposes, and for watching educational material',
+    'units_needed': 1,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/6/6e/DVD_Player_Yamaha_S540.JPG'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Ceiling Fans',
+    'descrip': 'Ceiling fans are required in the classrooms and in the dormitories',
+    'units_needed': 3,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/1/17/Ventilatore_a_soffitto_%283%29.png'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Cloth for Uniform',
+    'descrip': 'The kids currently do not have any uniforms, and moving towards one will be delightful, for them',
+    'units_needed': '-',
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/2/28/Indian_school_children_at_Hnahthial.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Large Trunks',
+    'descrip': 'Large trunks are needed for kids to store their belongings, safely. Currently, many have to share trunks or keep their belonging in the open',
+    'units_needed': 2,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/6/63/Baggage_cases_luggage_trunks_Epping-Ongar-Railway_North_Weald_Essex_England.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Carrom Board',
+    'descrip': 'Carrom Board will be a fun addition to the indoor activities, the kids indulge in.',
+    'units_needed': 1,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/4/4d/Carrom_board.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Volleyball Net',
+    'descrip': 'They could serve as general purpose nets for other sports like throwball, badminton, etc',
+    'units_needed': 2,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/2/29/Siatka.jpg'
 }, {
-    'title': 'Cricket Bats',
+    'title': 'Volleyballs',
     'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'units_needed': 2,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Beach_volleyball_ball.jpg'
 }, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
+    'title': 'Football Shoes',
+    'descrip': 'The kids are getting coached, and would love to play in proper football shoes',
+    'units_needed': 22,
+    'units_fulfilled': 0,
+    'cost': 0,
     'status': 0,
-    'imageURL': 'imgs/5.jpg'
-}, {
-    'title': 'Cricket Bats',
-    'descrip': '',
-    'units_needed': 10,
-    'units_fulfilled': 3,
-    'cost': 500,
-    'status': 0,
-    'imageURL': 'imgs/5.jpg'
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Adidas_Futbol_C.jpg'
 }];
-
 
 var genielist =[{
 	"name" : "Keerthana Gopalarishnan",
@@ -171,6 +171,7 @@ window.onload = function() {
         card.append($("<img></img>").attr("src", wishlist[i].imageURL).addClass("img-responsive tiles col-md-12"));
         card.append($("<p></p>").addClass("tile-title col-md-12").html(wishlist[i].title));
         card.append($("<p></p>").addClass("tile-descrip col-md-12").html(wishlist[i].descrip));
+        card.append($("<a></a>").addClass("tile-descrip col-md-12").attr("href", wishlist[i].link).html("Amazon/Flipkart link"));
         //card.append($("<button></button>").addClass("btn btn-danger buttony").html("Fulfill Wish <i class='fa fa-heart' aria-hidden='true'></i>"));
         var minicard = $("<div></div>").addClass("minicard1 col-md-12");
         var minicard1 = $("<div></div>").addClass("minicard1 col-md-6");
